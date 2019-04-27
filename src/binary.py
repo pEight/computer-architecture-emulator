5095c7ee4021cd87ce344c243a3b41fbddf04f3f
Phillipe Queiroz <phgqm7@gmail.com>
2019-04-27 14:20:05 -0300

    Created MIC-1 instruction class and a function that converts from decimal to binary.

---
+++ src/binary.py
@@ -0,0 +1,25 @@
+"""Uma função que converte um decimal para binário"""
+
+def convert_to_bin(int_value):
+  """Retorna um Dictionary contendo uma propriedade cujo o valor é uma
+  lista de 0's e 1's e a outra propriedade é uma string de 0's e 1's
+  
+  Keyword arguments:
+  int_value -- Um número inteiro em base decimal
+  """
+  bin_arr = []
+  leftover = 0
+  result = int_value
+
+  while(result >= 1):
+    leftover = int(result % 2)
+    bin_arr.append(leftover)
+    result = int(result / 2)
+
+  while(len(bin_arr) < 8):
+    bin_arr.append(0)
+
+  return {
+    "bin_arr": bin_arr[::-1],
+    "bin_str": "".join(map(str, bin_arr[::-1])) 
+  }
